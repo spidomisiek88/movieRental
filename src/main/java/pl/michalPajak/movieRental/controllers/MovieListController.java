58b242d07e963cbcefd873ffcfff34146607b0fb
spidomisiek88 <michal.pajak88@gmail.com>
2019-03-19 16:33:39 +0100

    k

--- src/main/java/pl/michalPajak/movieRental/controllers/MovieListController.java
+++ src/main/java/pl/michalPajak/movieRental/controllers/MovieListController.java
@@ -3,8 +3,14 @@ package pl.michalPajak.movieRental.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.michalPajak.movieRental.models.MovieForm;
 import pl.michalPajak.movieRental.services.MovieListService;
+
+import javax.validation.Valid;
 
 @Controller
 public class MovirListController {
@@ -18,5 +24,14 @@ public class MovirListController {
         model.addAttribute("movieList", movieListService.getAllMovies());
 
         return "movies_list_view";
+    }
+
+    @PostMapping("/movie/add")
+    public String addMovieToList(@ModelAttribute @Valid MovieForm movieForm, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            // TODO: 2019-03-19
+        }
+
+        return "movie_form";
     }
 }
